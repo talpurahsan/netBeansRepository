7fba63fd930cf13cd5e4fa35eba54263f398c96f
Ahsan Talpur <121304415+talpurahsan@users.noreply.github.com>
2022-12-25 11:57:01 +0500

    added "Hello Ahsan"
    added "Hello Ahsan" to GitDemo file

--- gitdemo/src/main/java/com/mycompany/gitdemo/Gitdemo.java
+++ gitdemo/src/main/java/com/mycompany/gitdemo/Gitdemo.java
@@ -9,6 +9,6 @@ public class Gitdemo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello GitHub");
+        System.out.println("Hello Ahsan");
     }
 }
